8152b78c3bb145d002311b08e677a2b60e3a0734
moshe <45424025+mmoshem@users.noreply.github.com>
2023-06-02 14:37:15 +0300

    gr

--- q2_2022_b_b.c
+++ q2_2022_b_b.c
@@ -2,6 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 5
+
+typedef struct Pair {
+    int i,j;
+}Pair;
+
+
+
+Pair* findAllfriendsofFriends(int* size, int** m, int sizeMat) {
+
+    int i, j, k, count = 0;
+    Pair* arr = (Pair*)malloc(sizeof(Pair) * ((N * N) / 2 - N));
+    for (i = 0; i < size; i++)
+        for (j = 0; j < i; j++)
+            if (m[i][j] == 0)
+                for (k = 0; k < size; k++)
+                    if (m[i][k] == 1 && m[k][j] == 1)
+                    {
+                        arr[count].i = i;
+                        arr[count].j = j;
+                        count++;
+                        break; //search for common friends of i and j has been terminated
+
+                    }
+
+}
+
+
+
 
 int main()
 {
