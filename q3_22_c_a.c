e31a96d0633d5575efc8eba5226e43c53cbe0dd4
moshe <45424025+mmoshem@users.noreply.github.com>
2023-06-04 09:47:35 +0300

    ghhgdfg

--- q3_22_c_a.c
+++ q3_22_c_a.c
@@ -3,33 +3,33 @@
 #include <string.h>
 
 
-
+//removes any close duplicates ex: 'aaa' -> 'a'   ,   '   '-> ' '   ,   'aa  ab  ' -> 'a ab '
 int removeCloseDupChars(char* str, char* str1) {
-	int i=1,j=1,dup=0, dup1 = 0;
+	int i=1,j=1,assholes=0, assholes1 = 0;
 	int len = strlen(str);
 	int len1 = strlen(str1);
 	while (str[i] != '\0') {
-		if (str[i]==' ' && str[i - 1]==' ') {
-			dup++;
+		if (str[i]==str[i - 1]) {
+			assholes++;
 		}
 		else {
-			str[i - dup] = str[i];
+			str[i - assholes] = str[i];
 		}
 		i++;
 	}
-	str[i - dup] = '\0';
+	str[i - assholes] = '\0';
 
 	while (str1[j] != '\0') {
-		if (str1[j] == ' ' && str1[j - 1] == ' ') {
+		if (str1[j] ==  str1[j - 1]) {
 
-			dup1++;
+			assholes1++;
 		}
 		else {
-			str1[j - dup1] = str1[j];
+			str1[j - assholes1] = str1[j];
 		}
 		j++;
 	}
-	str1[j - dup1] = '\0';
+	str1[j - assholes1] = '\0';
 
 
 	return (strcmp(str1, str) == 0 ? 1 : 0);
@@ -39,8 +39,8 @@ int removeCloseDupChars(char* str, char* str1) {
 
 int main() {
 
-	char str[] = "h  el  l o";
-	char str1[] = "h el l  o";
+	char str[] = "    stam BBdika   meSHUbaahaattttt  ";
+    char str1[] = " stammmm   BBdika meSHHHUbaaaaaahaattt ";
 	printf("%d",removeCloseDupChars(str, str1));
 	return 0;
 }
