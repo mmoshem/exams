7ddca56a1f1cf3f75ec29c6236f21184a703d843
moshe <45424025+mmoshem@users.noreply.github.com>
2023-06-05 14:01:47 +0300

    sdfsdf

--- q3_22_c_b.c
+++ q3_22_c_b.c
@@ -1,23 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 
 char* inverseString(char* str) {
 	int len = strlen(str);
-	int i=len-1,j=0;
+	int i = len - 1, j = 0, counter=1;
 	char* temp = (char*)calloc(len, sizeof(char));
-	memcpy(temp, str, len);
-	for (i,j; i >= 0; j++, i--) {
-		str[j] = temp[i];
+	if (!temp)return NULL;
+	//strcpy(temp, str);
+//	abc def'\0'
+//  0123456 7
+//      i
+	while (i >= 0) {
+		while (str[i] != ' ' && i > 0) {
+			counter++;
+			i--;
+		}
+		if (i == 0) {
+			
+			memcpy(temp + j, str + i, counter);
+			//memcpy_s(temp + j, counter, str + i, counter);
+			temp[len] = '\0';
+		}
+		else {
+			memcpy_s(temp + j, counter, str + i + 1, counter);
+			j += counter;
+		}
+		counter = 1;
+		while (str[--i] == ' ') {
+			i--;
+		}
+
 	}
-	return str; 
+	return temp; 
 }
 
 
 int main() {
 
-	char str[] = "hello world";
+	char str[] = "abc def";
 	char *str1 = inverseString(str);
 	printf("%s", str1);
 	return 0;
